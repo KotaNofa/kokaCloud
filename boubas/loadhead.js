0ea0b6a920cca21695dbfa1cbba5360408584015
Ari <79033264+KotaNofa@users.noreply.github.com>
2024-11-25 23:07:27 -0800

    Changed navbar and head to generic files

---
+++ boubas/loadhead.js
@@ -0,0 +1,6 @@
+fetch('/head.html')
+    .then(response => response.text())
+    .then(data => {
+        document.head.innerHTML += data;
+    })
+    .catch(error => console.error('Error loading head:', error));
